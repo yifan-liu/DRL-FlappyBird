84ec8be5ebfc7fa4dbbb0cd80613b69bd897ab1e
Zexuan Huang <zexuan_huang@brown.edu>
2017-12-03 14:44:55 -0500

    dqn with 14W model

--- dqn.py
+++ dqn.py
@@ -15,7 +15,7 @@ INITIAL_EPSILON = 0.1
 FINAL_EPSILON = 0.0001
 REPLAY_MEMORY = 50000
 BATCH_SIZE = 32
-FRAME_PER_ACTION = 3
+FRAME_PER_ACTION = 2
 LEARNING_RATE = 1e-6
 UPDATE_TIME = 100
 
@@ -55,18 +55,18 @@ class DQN:
         self.session.run(self.copy_target_network_op)
 
     def weight_variable(self,shape):
-        initial = tf.truncated_normal(shape, stddev = 0.01)
+        initial = tf.truncated_normal(shape,stddev=0.01)
         return tf.Variable(initial)
 
     def bias_variable(self,shape):
-        initial = tf.constant(0.01, shape = shape)
+        initial = tf.constant(0.01,shape=shape)
         return tf.Variable(initial)
 
     def conv2d(self,x, W, stride):
-        return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
+        return tf.nn.conv2d(x,W,strides=[1,stride,stride,1],padding="SAME")
 
     def max_pool_2x2(self,x):
-        return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
+        return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME")
 
     def create_network(self):
         # network weights
@@ -91,14 +91,14 @@ class DQN:
         state_input = tf.placeholder(tf.float32,[None,80,80,4])
 
         # hidden layers
-        h_conv1 = tf.nn.relu(self.conv2d(state_input,W_conv1,4) + b_conv1)
-        h_pool1 = self.max_pool_2x2(h_conv1)
+        h_conv1 = tf.nn.relu(self.conv2d(state_input,W_conv1,4) + b_conv1) # 【none,20,20,32]
+        h_pool1 = self.max_pool_2x2(h_conv1) # [none,10,10,32]
 
-        h_conv2 = tf.nn.relu(self.conv2d(h_pool1,W_conv2,2) + b_conv2)
+        h_conv2 = tf.nn.relu(self.conv2d(h_pool1,W_conv2,2) + b_conv2) # [none,5,5,64]
 
-        h_conv3 = tf.nn.relu(self.conv2d(h_conv2,W_conv3,1) + b_conv3)
+        h_conv3 = tf.nn.relu(self.conv2d(h_conv2,W_conv3,1) + b_conv3) # [none,5,5,64]
 
-        h_conv3_flat = tf.reshape(h_conv3,[-1,1600])
+        h_conv3_flat = tf.reshape(h_conv3,[-1,1600]) # [1,1600=5x5x64]
         h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat,W_fc1) + b_fc1)
 
         # Q Value layer
